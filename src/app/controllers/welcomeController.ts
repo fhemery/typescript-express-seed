30a3114924344888f02dfb7f14fe31a9a2214538
fhemery <fhemery@hemit.fr>
2017-09-19 12:19:02 +0200

    Added tests with mocha/chai, debugging capabilities through .vscode

--- src/app/controllers/welcomeController.ts
+++ src/app/controllers/welcomeController.ts
@@ -1,5 +1,5 @@
 // Import only what we need from express
-import { Router, Request, Response } from 'express';
+import { Request, Response, Router } from "express";
 
 // Assign router to the express.Router() instance
 const router: Router = Router();
@@ -7,9 +7,9 @@ const router: Router = Router();
 // The / here corresponds to the route that the WelcomeController
 // is mounted on in the server.ts file.
 // In this case it's /welcome
-router.get('/', (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
     // Reply with a hello world when no name param is provided
-    res.send('Hello, World!');
+    res.send("Hello, World!");
 });
 
 // Export the express.Router() instance to be used by server.ts
---
+++ src/test/server.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("Server", () => {
+    it("Should be able to add 2 and 2", () => {
+        expect(2 + 2).to.equal(4);
+    });
+});
